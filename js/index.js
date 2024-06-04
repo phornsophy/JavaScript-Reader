d0736042cb0b9835622cbca34d3c7fc60b6e3a2d
Sophy <phsophy2@gmail.com>
2024-06-04 22:48:33 +0700

    add new file

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+/*
 import { CardComponent } from "../components/CardComponent.js";
 import { products } from "../data/products.js";
 let renderArea = document.querySelector("#renderArea");
@@ -5,4 +6,15 @@ let renderArea = document.querySelector("#renderArea");
 // RENDER PRODUCT
 products
   .slice(0, 10)
-  .map((product) => (renderArea.innerHTML += CardComponent(product)));
+  .map((product) => (renderArea.innerHTML += CardComponent(product))); 
+*/
+
+import { NewCardComponent } from "../components/NewCardComponent.js";
+let renderArea = document.querySelector("#renderArea");
+
+const url = "http://127.0.0.1:5500/data/products.json";
+fetch(url).then((res) => (res.json())).then((data) => {
+  let products = data;
+  products.map((product) => renderArea.innerHTML += NewCardComponent(product));
+});
+
